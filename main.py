ea6f51e749e84ff70457b300281ffcafdb47ceee
adhikary97 <getspaceprod@gmail.com>
2023-06-21 00:26:44 -0700

    clean up

--- main.py
+++ main.py
@@ -19,14 +19,11 @@ def main():
     elif args.video and args.audio:
         parser.error("Only select one action --video or --audio")
 
-    video_id = str(uuid.uuid4())
-
     SetLogLevel(-1)
-
     model = Model(model_path="vosk-model-en-us-0.42-gigaspeech")
-
     logging.info("sp2t setup")
 
+    video_id = str(uuid.uuid4())
     os.makedirs(f"{video_id}")
 
     if args.audio != "":
